30b87cf1ecad9b657f194073bfc292cc4a4f1c2f
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2025-07-31 08:50:11 +0200

    Add docstrings

--- workflow/scripts/utils.py
+++ workflow/scripts/utils.py
@@ -1,10 +1,34 @@
-def get_bvals(bval_file):
+def get_bvals(bval_file: str) -> list[str]:
+    """Read b-values from a file to list of strings
+
+    Parameters
+    ----------
+    bval_file : str
+        path to bval file
+
+    Returns
+    -------
+    list[str]
+        b-values
+    """
     with open(bval_file, "r") as file:
         bvals = file.readline().strip().split(" ")
     return bvals
 
 
-def get_b0s(bval_file):
+def get_b0s(bval_file: str) -> int:
+    """Get number of 0 values in bval file
+
+    Parameters
+    ----------
+    bval_file : str
+        path to bval file
+
+    Returns
+    -------
+    int
+        number of 0 (or 0.0) values in bval file
+    """
     bvals = get_bvals(bval_file)
     b0s = len([x for x in bvals if float(x) == 0.0])
     return b0s
