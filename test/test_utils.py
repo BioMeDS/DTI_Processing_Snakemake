9699290010c9a76184a2b0462ae149a5f13d0d84
Markus J. Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2025-07-31 08:50:20 +0200

    Add test case

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,25 @@
-from workflow.scripts.utils import get_b0s
+from workflow.scripts.utils import get_b0s, get_bvals
+
+
+def test_get_bvals():
+    expected_bvals = [
+        "0",
+        "0.0",
+        "0",
+        "500",
+        "500",
+        "500",
+        "0",
+        "200",
+        "200.0",
+        "200",
+        "0",
+        "100",
+        "100",
+        "100",
+        "0",
+    ]
+    assert get_bvals("test/data/simple.bval") == expected_bvals
 
 
 def test_get_b0s():
